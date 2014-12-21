f5916648e2c08ef5c6652c88b1aa2694a083180b
Naveen <w0w.debuged@gmail.com>
2014-12-21 18:52:03 +0530

    updated post sample

--- postSmaple.sh
+++ postSmaple.sh
@@ -2,5 +2,5 @@ curl -X POST \
   -H "X-Parse-Application-Id: 1J6uMjrpOA3VmWIZFd1D0r2YTCFQcqCm2EqXdbGw" \
   -H "X-Parse-REST-API-Key: gIC0D9AHJQJy8izRId8LbPRxnxdOkK8fZmsutvLt" \
   -H "Content-Type: application/json" \
-  -d '{"score":1337,"deviceName":"Arduino","deviceType":"Micro Controller"}' \
+  -d '{"deviceName":"Arduino","deviceType":"Micro Controller"}' \
   https://api.parse.com/1/classes/DeviceList
