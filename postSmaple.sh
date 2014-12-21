e59be7699274bf70dd1222a96e79cd5206ed06b0
Naveen <w0w.debuged@gmail.com>
2014-12-21 18:46:35 +0530

    updated rest api key

--- postSmaple.sh
+++ postSmaple.sh
@@ -1,6 +1,6 @@
 curl -X POST \
   -H "X-Parse-Application-Id: 1J6uMjrpOA3VmWIZFd1D0r2YTCFQcqCm2EqXdbGw" \
-  -H "X-Parse-REST-API-Key: 25B6iRv5wAOY6qojAEmYYmtXakFU1ibX7g2QPbFK" \
+  -H "X-Parse-REST-API-Key: gIC0D9AHJQJy8izRId8LbPRxnxdOkK8fZmsutvLt" \
   -H "Content-Type: application/json" \
   -d '{"score":1337,"deviceName":"Arduino","deviceType":"Micro Controller"}' \
   https://api.parse.com/1/classes/DeviceList
